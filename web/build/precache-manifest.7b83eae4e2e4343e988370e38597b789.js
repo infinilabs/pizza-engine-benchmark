adb71f27b505d8be3734fe7b5bccaba35d9b6753
Paul Masurel <paul.masurel@gmail.com>
2020-10-17 23:34:28 +0900

    Removed coffeescript and gulp. Now using webpack

---
+++ web/build/precache-manifest.7b83eae4e2e4343e988370e38597b789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9164b33be1512d17d28307def4a4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b99aac2ce129db0a1a",
+    "url": "/static/css/main.efe263f6.chunk.css"
+  },
+  {
+    "revision": "c551a61abf2385a8f0bc",
+    "url": "/static/js/2.b9a6fc97.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/static/js/2.b9a6fc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b99aac2ce129db0a1a",
+    "url": "/static/js/main.c66c7816.chunk.js"
+  },
+  {
+    "revision": "e80fa3db5faf8870e46e",
+    "url": "/static/js/runtime-main.07d0b69d.js"
+  }
+]);
---
+++ web/src/index.js
@@ -0,0 +1,254 @@
+import React from 'react';
+import $ from 'jquery';
+import ReactDOM from 'react-dom';
+import './style.scss'
+import * as serviceWorker from './serviceWorker';
+
+function formatPercentVariation(p) {
+  if (p != undefined) {
+    return "+" + (p * 100).toFixed(1) + " %";
+  } else {
+    return "";
+  }
+}
+
+function numberWithCommas(x) {
+  var x = x.toString();
+  let pattern = /(-?\d+)(\d{3})/
+  while (pattern.test(x)) {
+    x = x.replace(pattern, "$1,$2");
+  }
+  return x;
+}
+
+function stats(timings) {
+  let median = timings[(timings.length / 2) | 0];
+  let mean = timings.reduce(((pv, cv) => pv + cv), 0) / timings.length;
+  return {
+    "median": median,
+    "mean": mean,
+    "min": timings[0],
+    "max": timings[timings.length - 1]
+  };
+}
+
+function aggregate(query) {
+  if (query.duration.length == 0) {
+    return { query: query.query, className: "unsupported", unsupported: true }
+  }
+  var res = stats(query.duration);
+  res.count = query.count;
+  res.query = query.query;
+  return res;
+}
+
+
+class Benchmark extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "TOP_10",
+      tag: null
+    };
+  }
+
+  handleChangeMode(evt) {
+    this.setState({ mode: evt.target.value });
+  }
+
+  handleChangeTag(evt) {
+    var tag = evt.target.value;
+    if (tag == "ALL") {
+      this.setState({ "tag": null });
+    } else {
+      this.setState({ "tag": tag });
+    }
+  }
+
+  filterQueries(queries) {
+    let tag = this.state.tag;
+    if (tag != undefined) {
+      return queries.filter(query => query.tags.indexOf(tag) >= 0);
+    } else {
+      return queries;
+    }
+  }
+
+  generateDataView() {
+    return {};
+  }
+
+  generateDataView() {
+    var engines = {}
+    var queries = {}
+    var mode_data = this.props.data[this.state.mode];
+    for (var engine in mode_data) {
+      var engine_queries = mode_data[engine];
+      engine_queries = Array.from(this.filterQueries(engine_queries));
+      engine_queries = engine_queries.map(aggregate);
+      var total = 0
+      var unsupported = false
+      for (var query of engine_queries) {
+        if (query.unsupported) {
+          unsupported = true;
+        } else {
+          total += query.min;
+        }
+      }
+      if (unsupported) {
+        total = undefined;
+      } else {
+        total = (total / engine_queries.length) | 0;
+      }
+      engines[engine] = total;
+      for (var query of engine_queries) {
+        var query_data = {};
+        if (queries[query.query] != undefined) {
+          query_data = queries[query.query];
+        }
+        query_data[engine] = query
+        queries[query.query] = query_data
+      }
+    }
+
+    for (var query in queries) {
+      var query_data = queries[query];
+      var min_engine = null;
+      var min_microsecs = 0;
+      var max_engine = null;
+      var max_microsecs = 0;
+      for (var engine in query_data) {
+        var engine_data = query_data[engine];
+        if (engine_data.unsupported)
+          continue;
+        if (min_engine == null || engine_data.min < min_microsecs) {
+          min_engine = engine;
+          min_microsecs = engine_data.min;
+        }
+        if (max_engine == null || engine_data.min > max_microsecs) {
+          max_engine = engine;
+          max_microsecs = engine_data.min;
+        }
+      }
+      for (var engine in query_data) {
+        var engine_data = query_data[engine];
+        if (engine_data.unsupported) continue;
+        if (engine != min_engine) {
+          engine_data.variation = (engine_data.min - min_microsecs) / min_microsecs;
+        }
+      }
+      if (min_engine != null) {
+        // Only useful if at least one engine supports this query 
+        query_data[min_engine].className = "fastest";
+        query_data[max_engine].className = "slowest";
+      }
+    }
+    return { engines, queries };
+  }
+
+  render() {
+    var data_view = this.generateDataView();
+    return <div>
+      <form>
+        <fieldset>
+          <label htmlFor="collectionField">Collection type</label>
+          <select id="collectionField" onChange={evt => this.handleChangeMode(evt)}>
+            {this.props.modes.map((mode) => <option value={mode} key={mode}>{mode}</option>)}
+          </select>
+          <label htmlFor="queryTagField">Type of Query</label>
+          <select id="queryTagField" onChange={(evt) => this.handleChangeTag(evt)}>
+            <option value="ALL" key="all">ALL</option>
+            {this.props.tags.map((tag) => <option value={tag} key={tag}>{tag}</option>)}
+          </select>
+        </fieldset>
+      </form>
+      <hr />
+      <table>
+        <thead>
+          <tr>
+            <th>Query</th>
+            {
+              Object.keys(data_view.engines).map((engine) => <th key={"col-" + engine}>{engine}</th>)
+            }
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="average-row">
+            <td>AVERAGE</td>
+            {
+              Object.entries(data_view.engines).map(kv => {
+                var engine = kv[0];
+                var engine_stats = kv[1];
+                if (engine_stats != undefined) {
+                  return <td key={"result-" + engine}>
+                    {numberWithCommas(engine_stats)} μs
+                </td>;
+                } else {
+                  return <td key={"result-" + engine}>
+                    Some Unsupported Queries
+                </td>;
+                }
+              })
+            }
+          </tr>
+          {
+            Object.entries(data_view.queries).map(kv => {
+              var query = kv[0];
+              var engine_queries = kv[1];
+              return <tr>
+                <td>{query}</td>
+                {
+                  Object.keys(data_view.engines).map(engine => {
+                    var cell_data = engine_queries[engine];
+                    if (cell_data.unsupported) {
+                      return <td className={"data " + cell_data.className}></td>;
+                    } else {
+                      return <td className={"data " + cell_data.className}>
+                        <div className="timing">{numberWithCommas(cell_data.min)}  μs</div>
+                        <div className="timing-variation">{formatPercentVariation(cell_data.variation)}</div>
+                        <div className="count">{numberWithCommas(cell_data.count)} docs</div>
+                      </td>;
+                    }
+                  })
+                }
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+    </div>;
+  }
+
+}
+
+$(function () {
+  $.getJSON(process.env.PUBLIC_URL + "/results.json", (data) => {
+    var modes = [];
+    var engines = [];
+    var tags_set = new Set();
+    var tags = [];
+    for (var mode in data) {
+      modes.push(mode);
+    }
+    for (var engine in data[modes[0]]) {
+      engines.push(engine);
+    }
+    for (var query of data[modes[0]][engines[0]]) {
+      for (var tag of query.tags) {
+        tags_set.add(tag);
+      }
+    }
+    var tags = Array.from(tags_set);
+    tags.sort();
+    var el = document.getElementById("app-container");
+    ReactDOM.render(<React.StrictMode>
+      <Benchmark data={data} tags={tags} modes={modes} engines={engines} />
+    </React.StrictMode>, el);
+  });
+});
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
